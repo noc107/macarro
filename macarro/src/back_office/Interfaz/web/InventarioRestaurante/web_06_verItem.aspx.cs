230f3e9b8a45fc1e549c1cad8379eeaaeb467556
Oswaldo Zarate <Oswaldo Zarate@localhost>
2014-12-05 17:24:55 -0530

    TryCatch VerItem Interfaz

--- macarro/src/back_office/Interfaz/web/InventarioRestaurante/web_06_verItem.aspx.cs
+++ macarro/src/back_office/Interfaz/web/InventarioRestaurante/web_06_verItem.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using back_office.Logica.InventarioRestaurante;
+using back_office.Excepciones.InventarioRestaurante;
 
 namespace back_office.Interfaz.web.InventarioRestaurante
 {
@@ -13,23 +14,39 @@ namespace back_office.Interfaz.web.InventarioRestaurante
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                ProcedimientosItem _procedimiento = new ProcedimientosItem();
+                string[] _mostrar = _procedimiento.verItem(10);
 
-            ProcedimientosItem _procedimiento = new ProcedimientosItem();
-            string [] _mostrar = _procedimiento.verItem(10);
-
-            lbNombreVer.Text = _mostrar[0];
-            lbPrecio2.Text =_mostrar[1];
-            lbProveedor.Text = _mostrar[2];
-            lbCantidadVer.Text = _mostrar[3];
-            lbDescripcionVer.Text= _mostrar[4];
-            lbPrecio.Text = _mostrar[5];
+                lbNombreVer.Text = _mostrar[0];
+                lbPrecio2.Text = _mostrar[1];
+                lbProveedor.Text = _mostrar[2];
+                lbCantidadVer.Text = _mostrar[3];
+                lbDescripcionVer.Text = _mostrar[4];
+                lbPrecio.Text = _mostrar[5];
 
 
-            ListItem list = new ListItem("Cantidad: "+ _mostrar[6]+ "Fecha: " + _mostrar[7]);
-            ListBox1.Items.Add(list);
+                ListItem list = new ListItem("Cantidad: " + _mostrar[6] + "Fecha: " + _mostrar[7]);
+                ListBox1.Items.Add(list);
 
-          //  ListBox1.Items.AddRange(string[]);
-
+                //  ListBox1.Items.AddRange(string[]);
+            }
+            catch (ExcepcionVerItem)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(),
+          "err_msg",
+          "alert('Ha ocurrido un error de base de datos, por favor intente luego)');",
+          true);
+                Response.Redirect("web_06_gestionarInventario.aspx");
+            }
+            catch (Exception) 
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(),
+          "err_msg",
+          "alert('Ha ocurrido un error)');",
+          true);
+            }
 
         }
 
