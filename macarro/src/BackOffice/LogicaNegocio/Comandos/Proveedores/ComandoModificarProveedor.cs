60b8da13363b1cfdce25e170663d8c73b4fc62e5
Cesar <Cesar@localhost>
2015-01-18 16:46:09 -0530

    Comando Modificar

---
+++ macarro/src/BackOffice/LogicaNegocio/Comandos/Proveedores/ComandoModificarProveedor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BackOffice.Dominio;
+using BackOffice.FuenteDatos.Fabrica;
+using BackOffice.FuenteDatos.IDao.Proveedores;
+
+namespace BackOffice.LogicaNegocio.Comandos.Proveedores
+{
+    public class ComandoModificarProveedor : Comando<Entidad, bool>
+    {
+        public override bool Ejecutar(Entidad parametro)
+        {
+            try
+            {
+
+                IDaoProveedor _daoProveedor;
+                _daoProveedor = FabricaDao.ObtenerDaoProveedor();
+                return _daoProveedor.Modificar(parametro);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return false;
+        }
+    }
+}
